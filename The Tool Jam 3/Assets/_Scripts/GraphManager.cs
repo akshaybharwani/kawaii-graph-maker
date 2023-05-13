ce8c7a51838bf0f4239c766d3a7e8f6bb7637ce0
Akshay Bharwani <akshaybharwani@pm.me>
2023-05-13 10:56:45 +0900

    Improve Axis Lines and add basic bar UI layout

--- The Tool Jam 3/Assets/_Scripts/GraphManager.cs
+++ The Tool Jam 3/Assets/_Scripts/GraphManager.cs
@@ -4,10 +4,18 @@ public class GraphManager : MonoBehaviour
 {
     [SerializeField]
     private BarController barPrefab;
+
+    [SerializeField] 
+    private Transform barParent;
+    
+    private void Start()
+    {
+        
+    }
     
     public void AddBar()
     {
-        
+        var bar = Instantiate(barPrefab, barParent);
     }
 
     public void RemoveBar()

dd2483ed2d7ed7c6a70057bf116a6d8733c1a8e0
Akshay Bharwani <akshaybharwani@pm.me>
2023-05-13 11:46:43 +0900

    Add basic prefabs for UI Control panels

--- The Tool Jam 3/Assets/_Scripts/GraphManager.cs
+++ The Tool Jam 3/Assets/_Scripts/GraphManager.cs
@@ -1,13 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GraphManager : MonoBehaviour
 {
+    public static GraphManager Instance;
+ 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    
     [SerializeField]
     private BarController barPrefab;
 
     [SerializeField] 
     private Transform barParent;
-    
+
+    private List<BarController> bars = new List<BarController>();
+    public List<BarController> Bars => bars;
+
     private void Start()
     {
         
@@ -16,10 +31,23 @@ public class GraphManager : MonoBehaviour
     public void AddBar()
     {
         var bar = Instantiate(barPrefab, barParent);
+        bars.Add(bar);
     }
 
-    public void RemoveBar()
+    public void RemoveBar(BarController barToRemove)
     {
-        
+        if (barToRemove && bars.Count > 0)
+        {
+            bars.Remove(barToRemove);
+            Destroy(barToRemove);
+        }
+    }
+
+    public void RemoveLastBar()
+    {
+        if (bars.Count > 0)
+        {
+            bars.Remove(bars.Last());
+        }
     }
 }

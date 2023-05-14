9098b55d2896b57c321422173d2cf4ffbb6a18eb
Akshay Bharwani <akshaybharwani@pm.me>
2023-05-14 14:14:03 +0900

    Add screen size factor calculation for screenshot

--- The Tool Jam 3/Assets/_Scripts/Utilities.cs
+++ The Tool Jam 3/Assets/_Scripts/Utilities.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows;
+using UnityEngine.UI;
+using Screen = UnityEngine.Device.Screen;
 
 public class Utilities : MonoBehaviour
 {
@@ -25,17 +25,36 @@ public class Utilities : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
         
-        // Capture rect
+        // Capture rect corners
         var rectWorldCorners = new Vector3[4];
         rectTransform.GetWorldCorners(rectWorldCorners);
         var rectLeftCorner = rectWorldCorners[0];
+        
+        // Screen comparison
+        Debug.Log(Screen.height);
+        var canvasResolution = rectTransform.GetComponentInParent<CanvasScaler>().referenceResolution;
+        var xFactor = Screen.width / canvasResolution.x;
+        var yFactor = Screen.height / canvasResolution.y;
+        
         var rect = rectTransform.rect;
-        var rectWidth = rect.width;
-        var rectHeight = rect.height;
+        var rectWidth = rect.width * xFactor;
+        var rectHeight = rect.height * yFactor;
         var rectTexture = new Texture2D((int)rectWidth, (int)rectHeight, TextureFormat.RGB24, false);
         rectTexture.ReadPixels(new Rect(rectLeftCorner.x, rectLeftCorner.y, rectWidth, rectHeight), 0, 0);
         rectTexture.Apply();
         var bytes = rectTexture.EncodeToPNG();
-        File.WriteAllBytes($"{Application.dataPath}/Kawaii Graph_{DateTime.Now:hh-mm-ss}.png", bytes);
+#if UNITY_WEBGL
+        if (WebGLFileSaver.IsSavingSupported())
+        {
+            WebGLFileSaver.SaveFile(bytes, "Kawaii Graph_{DateTime.Now:hh-mm-ss}.png");
+        }
+        else
+        {
+            // TODO: Show Error Dialog Box
+        }
+#endif
+#if UNITY_EDITOR
+        System.IO.File.WriteAllBytes($"{Application.dataPath}/Kawaii Graph_{DateTime.Now:hh-mm-ss}.png", bytes);
+#endif
     }
 }

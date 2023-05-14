262012018e1b5eaee0458dbc878d121fcc1f26f6
Akshay Bharwani <akshaybharwani@pm.me>
2023-05-14 18:23:59 +0900

    Add Tags for all objects for changing theme

--- The Tool Jam 3/Assets/_Scripts/ThemeManager.cs
+++ The Tool Jam 3/Assets/_Scripts/ThemeManager.cs
@@ -1,12 +1,22 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThemeManager : MonoBehaviour
 {
+    [Header("Theme stuff")]
     [SerializeField] 
     private ThemeData themeData;
 
     [SerializeField] 
     private Transform themeParent;
+    
+    [SerializeField] 
+    private GameObject iconTextButton;
+    
+    /*[Header("Tool objects")]
+    [SerializeField]
+    private */
 
     private void Start()
     {
@@ -14,6 +24,16 @@ public class ThemeManager : MonoBehaviour
     }
 
     private void Populate()
+    {
+        for (var index = 0; index < themeData.themes.Count; index++)
+        {
+            var themeButton = Instantiate(iconTextButton, themeParent);
+            themeButton.GetComponent<TextMeshProUGUI>().text = (index + 1).ToString();
+            themeButton.GetComponent<Button>().onClick.AddListener(delegate { ChangeTheme(index); });
+        }
+    }
+
+    private void ChangeTheme(int themeIndex)
     {
         
     }

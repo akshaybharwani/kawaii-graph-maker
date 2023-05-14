d2e721285e6b05048b62377cde76beb16fa7ab46
Akshay Bharwani <akshaybharwani@pm.me>
2023-05-14 13:38:32 +0900

    Improve screenshot code

--- The Tool Jam 3/Assets/_Scripts/Utilities.cs
+++ The Tool Jam 3/Assets/_Scripts/Utilities.cs
@@ -27,17 +27,14 @@ public class Utilities : MonoBehaviour
         
         // Capture rect
         var rectWorldCorners = new Vector3[4];
-        var rect = rectTransform.rect;
         rectTransform.GetWorldCorners(rectWorldCorners);
-        var rectTexture = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
-        rectTexture.ReadPixels(new Rect(rectWorldCorners[0].x, rectWorldCorners[0].y, rect.width, rect.height), 0, 0);
+        var rectLeftCorner = rectWorldCorners[0];
+        var rect = rectTransform.rect;
+        var rectWidth = rect.width;
+        var rectHeight = rect.height;
+        var rectTexture = new Texture2D((int)rectWidth, (int)rectHeight, TextureFormat.RGB24, false);
+        rectTexture.ReadPixels(new Rect(rectLeftCorner.x, rectLeftCorner.y, rectWidth, rectHeight), 0, 0);
         rectTexture.Apply();
-
-        // Capture screen and put rect in the middle
-        /*var texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        texture.Apply();*/
-        
         var bytes = rectTexture.EncodeToPNG();
         File.WriteAllBytes($"{Application.dataPath}/Kawaii Graph_{DateTime.Now:hh-mm-ss}.png", bytes);
     }
